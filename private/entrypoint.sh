a83f0704b0202e73c9ca89090041b719bccf6650
Dmytro Titov <dmytro.titov@gmail.com>
2019-03-06 15:18:05 +0100

    Use shovels instead of federations between public and private

--- private/entrypoint.sh
+++ private/entrypoint.sh
@@ -2,6 +2,5 @@
 
 sed -i 's%USER_NAME%'${USER_NAME}'%g' /etc/rabbitmq/definitions.json
 sed -i 's%PASSWORD_HASH%'${PASSWORD_HASH}'%g' /etc/rabbitmq/definitions.json
-sed -i 's%PUBLIC_CONNECTION%'${PUBLIC_CONNECTION}'%g' /etc/rabbitmq/definitions.json
 
 exec /usr/local/bin/docker-entrypoint.sh "$@"
