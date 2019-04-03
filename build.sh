621996f06fa7acc192f9d5a7508584fe8cdc65eb
Dmytro Titov <dmytrot@ifi.uio.no>
2019-04-03 13:04:48 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t uiobmi/localega-broker:private private
+docker build -t uiobmi/localega-broker-private private
 
-docker build -t uiobmi/localega-broker:public public
+docker build -t uiobmi/localega-broker-public public
