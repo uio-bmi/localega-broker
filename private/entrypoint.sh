6cac4023432143d1760ea742052d574828621499
Dmytro Titov <dmytro.titov@gmail.com>
2019-05-06 12:30:03 +0200

    Use ! instead of % in sed

--- private/entrypoint.sh
+++ private/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i 's%USER_NAME%'${USER_NAME}'%g' /etc/rabbitmq/definitions.json
-sed -i 's%PASSWORD_HASH%'${PASSWORD_HASH}'%g' /etc/rabbitmq/definitions.json
+sed -i 's!USER_NAME!'${USER_NAME}'!g' /etc/rabbitmq/definitions.json
+sed -i 's!PASSWORD_HASH!'${PASSWORD_HASH}'!g' /etc/rabbitmq/definitions.json
 
 exec /usr/local/bin/docker-entrypoint.sh "$@"
