2cb644f1fefb20bbd46ad2dac58b23256e31b625
Dmytro Titov <dmytro.titov@gmail.com>
2019-08-21 13:37:34 +0200

    Add UH deployment

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+  curl $TRYGGVE_CA --create-dirs -o ~/.docker/ca.pem
+  curl $TRYGGVE_CERT --create-dirs -o ~/.docker/cert.pem
+  curl $TRYGGVE_KEY --create-dirs -o ~/.docker/key.pem
+  docker stack rm MQ
+  docker config rm CA.cert
+  docker config rm mq.cert
+  docker config rm mq.key
+  docker config create CA.cert CA.cert
+  docker config create mq.cert mq.cert
+  docker config create mq.key mq.key
+  docker build -t uiobmi/localega-broker-public:latest public
+  docker stack deploy MQ --compose-file docker-stack.yml
+#fi
