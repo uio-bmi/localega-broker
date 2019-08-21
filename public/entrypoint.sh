fca59d9a1db29af35516b91fa033299c497d79e5
Dmytro Titov <dmytro.titov@gmail.com>
2019-08-21 14:44:54 +0200

    Use PERL instead of sed

--- public/entrypoint.sh
+++ public/entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-sed -i 's!USER_NAME!'${USER_NAME}'!g' /etc/rabbitmq/definitions.json
-sed -i 's!PASSWORD_HASH!'${PASSWORD_HASH}'!g' /etc/rabbitmq/definitions.json
-sed -i 's!CEGA_CONNECTION!'${CEGA_CONNECTION}'!g' /etc/rabbitmq/definitions.json
-sed -i 's!CEGA_CONNECTION!'${CEGA_CONNECTION}'!g' /etc/rabbitmq/advanced.config
-#sed -i 's!PRIVATE_CONNECTION!'${PRIVATE_CONNECTION}'!g' /etc/rabbitmq/advanced.config
-perl -i.bak -pe 's!PRIVATE_CONNECTION!'${PRIVATE_CONNECTION}'!g' /etc/rabbitmq/advanced.config
+perl -i.bak -pe 's!USER_NAME!$ENV{"USER_NAME"}!g' /etc/rabbitmq/definitions.json
+perl -i.bak -pe 's!PASSWORD_HASH!$ENV{"PASSWORD_HASH"}!g' /etc/rabbitmq/definitions.json
+perl -i.bak -pe 's!CEGA_CONNECTION!$ENV{"CEGA_CONNECTION"}!g' /etc/rabbitmq/definitions.json
+perl -i.bak -pe 's!CEGA_CONNECTION!$ENV{"CEGA_CONNECTION"}!g' /etc/rabbitmq/advanced.config
+perl -i.bak -pe 's!PRIVATE_CONNECTION!$ENV{"PRIVATE_CONNECTION"}!g' /etc/rabbitmq/advanced.config
 
 exec /usr/local/bin/docker-entrypoint.sh "$@"
