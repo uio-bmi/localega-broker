d10f3cd651377fa41972b64887d74608dbfc1b36
Dmytro Titov <dmytro.titov@gmail.com>
2019-08-21 14:17:38 +0200

    Use PERL instead of sed

--- public/entrypoint.sh
+++ public/entrypoint.sh
@@ -4,6 +4,7 @@ sed -i 's!USER_NAME!'${USER_NAME}'!g' /etc/rabbitmq/definitions.json
 sed -i 's!PASSWORD_HASH!'${PASSWORD_HASH}'!g' /etc/rabbitmq/definitions.json
 sed -i 's!CEGA_CONNECTION!'${CEGA_CONNECTION}'!g' /etc/rabbitmq/definitions.json
 sed -i 's!CEGA_CONNECTION!'${CEGA_CONNECTION}'!g' /etc/rabbitmq/advanced.config
-sed -i 's!PRIVATE_CONNECTION!'${PRIVATE_CONNECTION}'!g' /etc/rabbitmq/advanced.config
+#sed -i 's!PRIVATE_CONNECTION!'${PRIVATE_CONNECTION}'!g' /etc/rabbitmq/advanced.config
+perl -i.bak -pe 's!PRIVATE_CONNECTION!'${PRIVATE_CONNECTION}'!g' /etc/rabbitmq/advanced.config
 
 exec /usr/local/bin/docker-entrypoint.sh "$@"
