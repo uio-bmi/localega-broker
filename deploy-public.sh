dd9c206007fa070e6be7312a3323bbd7a3319b4c
Dmytro Titov <dmytro.titov@gmail.com>
2019-08-21 16:06:08 +0200

    Add private broker deployment

--- deploy-public.sh
+++ deploy-public.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+  rm -rf ~/.docker
+  export DOCKER_HOST=$DOCKER_HOST_TRYGGVE
   curl $TRYGGVE_CA --create-dirs -o ~/.docker/ca.pem
   curl $TRYGGVE_CERT --create-dirs -o ~/.docker/cert.pem
   curl $TRYGGVE_KEY --create-dirs -o ~/.docker/key.pem
@@ -9,8 +11,8 @@ if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
   docker config rm mq.cert
   docker config rm mq.key
   docker config create CA.cert CA.cert
-  docker config create mq.cert mq.cert
-  docker config create mq.key mq.key
+  docker config create mq.cert mq-public.cert
+  docker config create mq.key mq-public.key
   docker build --no-cache -t uiobmi/localega-broker-public:uh public
-  docker stack deploy MQ --compose-file docker-stack.yml
+  docker stack deploy MQ --compose-file docker-stack-public.yml
 fi
