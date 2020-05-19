f07e3c2a609ee66fa2bde4187df8a8af70362559
dtitov <dmytro.titov@gmail.com>
2020-05-19 19:52:50 +0200

    Expose more SSL-related options

--- private/entrypoint.sh
+++ private/entrypoint.sh
@@ -2,5 +2,8 @@
 
 perl -i.bak -pe 's!USER_NAME!$ENV{"USER_NAME"}!g' /etc/rabbitmq/definitions.json
 perl -i.bak -pe 's!PASSWORD_HASH!$ENV{"PASSWORD_HASH"}!g' /etc/rabbitmq/definitions.json
+perl -i.bak -pe 's!SSL_VERIFY!$ENV{"SSL_VERIFY"}!g' /etc/rabbitmq/rabbitmq.conf
+perl -i.bak -pe 's!SSL_FAIL_IF_NO_PEER_CERT!$ENV{"SSL_FAIL_IF_NO_PEER_CERT"}!g' /etc/rabbitmq/rabbitmq.conf
+perl -i.bak -pe 's!SSL_DEPTH!$ENV{"SSL_DEPTH"}!g' /etc/rabbitmq/rabbitmq.conf
 
 exec /usr/local/bin/docker-entrypoint.sh "$@"
